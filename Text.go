75e857f0a15890163ef9d7b5827002fbf9cd7e6c
bitsbuster <thebitsbuster@gmail.com>
2024-10-06 21:05:57 +0200

    reverse string

--- Text.go
+++ Text.go
@@ -52,3 +52,12 @@ func FindSubstringOcurrences(text, substring string) ([]int, error) {
 	return appears, nil
 
 }
+
+// ReverseString reverses a string
+func ReverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
